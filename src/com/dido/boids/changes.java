b737f99815710860d9c26340195426abb71f8f47
Dido Nenov <dido.nenov@gmail.com>
2014-02-11 14:31:48 +0000

    Added PApplet

--- src/com/dido/boids/changes.java
+++ src/com/dido/boids/changes.java
@@ -3,6 +3,12 @@ package com.dido.boids;
 import processing.core.*;
 import controlP5.*;
 
-public class changes {
+public class changes extends PApplet {
 
+	int j = 12324;
+	
+	public void setup() {}
+	
+	public void draw() {}
+	
 }
